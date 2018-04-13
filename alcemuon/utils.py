617b190f257277daab08dbd102a5a8322bb3666f
stur86 <simonesturniolo@gmail.com>
2018-04-13 12:37:11 +0100

    Created fundamental structure, updated license, started copying code in utils.py

---
+++ alcemuon/utils.py
@@ -0,0 +1,40 @@
+# ALCe by Simone Sturniolo
+# Copyright (C) 2018 - Science and Technology Facility Council
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Module containing utility functions"""
+
+# Python 2-to-3 compatibility code
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+
+
+def multikron(*m):
+    # Recursive Kronecker product for multiple matrices
+    if len(m) == 1:
+        return m[0]
+    elif len(m) == 2:
+        return np.kron(*m)
+    else:
+        return np.kron(m[0], multikron(*m[1:]))
